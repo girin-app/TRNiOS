0a38bde66c76a906109933843c29356d25ae889a
JoowonYun <joowon@girin.app>
2024-07-30 18:28:28 +0900

    feat: support to fee proxy in bridge

--- Sources/TRNiOS/Api/Types/Method.swift
+++ Sources/TRNiOS/Api/Types/Method.swift
@@ -24,3 +24,22 @@ struct WithdrawXrpArgs {
     let amount: EthereumQuantity
     let destination: EthereumAddress
 }
+
+struct MethodFeeProxy: Method {
+    let callIndex: [UInt8] = Data(hex: "1f00").bytes
+    let args: FeeProxyArgs
+
+    func toU8a() -> [UInt8] {
+        var u8a = callIndex
+        u8a.append(contentsOf: bnToU8a(bn: args.paymentAsset, bitLength: 32))
+        u8a.append(contentsOf: bnToU8a(bn: args.maxPayment, bitLength: 128))
+        u8a.append(contentsOf: args.call.toU8a())
+        return u8a
+    }
+}
+
+struct FeeProxyArgs {
+    let paymentAsset: BigUInt
+    let maxPayment: BigUInt
+    let call: MethodWithdrawXrp
+}
