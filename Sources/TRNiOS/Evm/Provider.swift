b8373c1738d3e061445e9184bd455fe53d29d62a
Minji Kim <mimin@girin.app>
2024-08-30 01:13:23 +0900

    feat: make functions as public

--- Sources/TRNiOS/Evm/Provider.swift
+++ Sources/TRNiOS/Evm/Provider.swift
@@ -2,14 +2,14 @@ import Foundation
 import Web3
 import BigInt
 
-enum NetworkName: String {
+public enum NetworkName: String {
     case root
     case porcini
 }
 
-typealias HttpProviderUrl = String
+public typealias HttpProviderUrl = String
 
-func getPublicProviderUrl(network: NetworkName) -> HttpProviderUrl {
+public func getPublicProviderUrl(network: NetworkName) -> HttpProviderUrl {
     switch network {
     case .root:
         return "https://root.rootnet.live/archive"
@@ -20,6 +20,6 @@ func getPublicProviderUrl(network: NetworkName) -> HttpProviderUrl {
     }
 }
 
-func getWeb3Provider(url: String, networkName: NetworkName) -> Web3HttpProvider {
+public func getWeb3Provider(url: String, networkName: NetworkName) -> Web3HttpProvider {
     return Web3HttpProvider(rpcURL: url)
 }
